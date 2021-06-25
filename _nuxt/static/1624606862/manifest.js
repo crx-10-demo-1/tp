abea632c14d33bd7a5bf50630dfca17fee080095
tsukasa.arita.ye <tsukasa.arita.ye@m-tec.co.jp>
2021-06-25 16:41:33 +0900

    画像サイズ変更、文字変更

---
+++ _nuxt/static/1624606862/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fjob","\u002Fpalette","\u002Falarm","\u002F"]})
---
+++ _nuxt/static/1624606862/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Ftp\u002F_nuxt\u002Fstatic\u002F1624606862",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{STATIC_URL:b,_app:{basePath:b,assetsPath:"\u002Ftp\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002Ftp\u002F"));
